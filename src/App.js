60674ebccac10ca848a5aae5f837ef6de3e20e6d
akiraah <rah24.a@gmail.com>
2022-08-02 07:23:46 +0100

    landing page styling + logo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './resource/ziggo-logo.svg'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <section className="one">
+        <img src={logo} className="survey-logo" alt="logo" />
+      </section>
+      <section className="two">
+        <h1>Two</h1>
+      </section>
     </div>
   );
 }
